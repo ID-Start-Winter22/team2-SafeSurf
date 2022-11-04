fbebaae73624d4fae8a5102d4c993643dff1a6ac
wiesty <basti@hypermail.de>
2022-11-04 11:30:45 +0100

    python wetter test

--- actions/weather.py
+++ actions/weather.py
@@ -1,56 +1,68 @@
-import pip._vendor.requests, json
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+# import required modules
+import requests, json
 
+# Enter your API key here
+api_key = "0ad3ff8e75c082c6fc193348b16958b1 "
 
-class ActionHelloWorld(Action):
-
-     def name(self) -> Text:
-         return "action_tell_weather"
-
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-         dispatcher.utter_message(text="Wie ist das Wetter?")
-
-         return []
-
-api_key = "0ad3ff8e75c082c6fc193348b16958b1"
-
+# base_url variable to store url
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
+# Give city name
 city_name = input("Enter city name : ")
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric" + "&lang=de"
-response = pip._vendor.requests.get(complete_url)
+
+# complete_url variable to store
+# complete url address
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+# get method of requests module
+# return response object
+response = requests.get(complete_url)
+
+# json method of response object
+# convert json format data into
+# python format data
 x = response.json()
 
+# Now x contains list of nested dictionaries
+# Check the value of "cod" key is equal to
+# "404", means city is found otherwise,
+# city is not found
 if x["cod"] != "404":
 
-    y = x["main"]
+	# store the value of "main"
+	# key in variable y
+	y = x["main"]
 
-    current_temperature = y["temp"]
-    current_pressure = y["pressure"]
-    current_humidity = y["humidity"]
-    z = x["weather"]
-    weather_description = z[0]["description"]
-    print(
-        "\n " +
-        "Temperature = " +
-                    str(current_temperature) + "°C" +
-        "\n " +
-        "\n Luftdruck = " +
-                    str(current_pressure) + " hPa" +
-                "\n " +
-        "\n Humidity = " +
-                    str(current_humidity) + "%" +
-                "\n " +
-        "\n Beschreibung = " +
-                    str(weather_description) +
-        "\n " +
-        "\n Have a Nice Day!!!! " +
-        "\n " )
+	# store the value corresponding
+	# to the "temp" key of y
+	current_temperature = y["temp"]
+
+	# store the value corresponding
+	# to the "pressure" key of y
+	current_pressure = y["pressure"]
+
+	# store the value corresponding
+	# to the "humidity" key of y
+	current_humidity = y["humidity"]
+
+	# store the value of "weather"
+	# key in variable z
+	z = x["weather"]
+
+	# store the value corresponding
+	# to the "description" key at
+	# the 0th index of z
+	weather_description = z[0]["description"]
+
+	# print following values
+	print(" Temperature (in kelvin unit) = " +
+					str(current_temperature) +
+		"\n atmospheric pressure (in hPa unit) = " +
+					str(current_pressure) +
+		"\n humidity (in percentage) = " +
+					str(current_humidity) +
+		"\n description = " +
+					str(weather_description))
 
 else:
-    print(" City Not Found ")
+	print(" City Not Found ")
