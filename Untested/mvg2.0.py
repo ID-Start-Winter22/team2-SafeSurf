e6689b64775dd39e7c183aa42f1c393a76cb11b5
MauliQT <87561264+MauliQT@users.noreply.github.com>
2022-11-28 09:07:35 +0100

    Update mvg2.0.py
    works now but i dont get time in hour format :/

--- Untested/mvg2.0.py
+++ Untested/mvg2.0.py
@@ -13,12 +13,6 @@ import json
 MVG_STATION_URL = "https://www.mvg.de/api/fahrinfo/location/queryWeb"
 MVG_ROUTING_URL = "https://www.mvg.de/api/fahrinfo/routing"
 
-start = input("Start ")
-destination = input("Destination ")
-
-from_station = start
-to_sation = destination
-
 def name_to_station(name: str):
     mvg_resp = requests.get(MVG_STATION_URL, params={"q": name})
     
@@ -36,55 +30,44 @@ def name_to_station(name: str):
     return station
 
 # Returns travel time in minutes
-def get_travel_time_for_stationIDs(start, destination):
-    mvg_resp = requests.get(MVG_STATION_URL, params={ "Station": start, "Sation": destination})
+def get_travel_time_for_stationIDs(station_a, station_b):
+    mvg_resp = requests.get(MVG_ROUTING_URL, params={ "fromStation": station_a, "toStation": station_b })
 
-    if mvg_resp.satus_code != requests.codes.ok:
+    if mvg_resp.status_code != requests.codes.ok:
         return None
-    
 
     as_dict = mvg_resp.json()
 
     travel_time = None
-    train_type = None
     departure_time = None
     arrival_time = None
-
-    for connection in as_dict["connectionList"]:
+    for connection in as_dict["connectionList"]: 
         departure_time = connection["departure"]
-        arrival_time   = connection["arrival"]
+        arrival_time   = connection["arrival"] 
+        arrival = arrival_time / 1000.0 / 60.0 / 60.0 / 24 
+        departure = departure_time / 1000.0 / 60.0 / 60.0 / 24 
         delta_time_ms = arrival_time - departure_time
         travel_time   = (delta_time_ms / 1000.0) / 60.0
-        train_type = ["list"]
         break
 
-    return train_type + arrival_time + departure_time + travel_time
+    return travel_time, departure, arrival
 
 def handle_route(start, destination):
     from_station  = name_to_station(start)
     to_station    = name_to_station(destination)
 
     if from_station and to_station:
-        print("Route von " + from_station["id"] + " zu " + to_station["id"]+ " wird geprüft")
+        print("Checking route from " + from_station["name"], from_station["id"] + " to " + to_station["name"], to_station["id"])
     else:
         return "At least one unknown station!"
-
-    departure_time = get_travel_time_for_stationIDs(from_station["id"])
-    if departure_time:
-        print("Departure Time: ", departure_time)
-    else:
-        return "Could not calculate departure-time for this pair of stations!"
-
-    arrival_time = get_travel_time_for_stationIDs(to_station["id"])
-    if arrival_time:
-        print("Arrival Time: ", arrival_time)
-    else:
-        return "Could not calculate arrival-time for this pair of stations!"
-
+    
     travel_time = get_travel_time_for_stationIDs(from_station["id"], to_station["id"])
     if travel_time:
         print("Time needed:", travel_time, "min")
     else:
         return "Could not calculate travel-time for this pair of stations!"
 
-print(handle_route(start, destination))
+start = input("Start ")
+ende = input("Endstation ")
+# Test
+handle_route(start, ende)
